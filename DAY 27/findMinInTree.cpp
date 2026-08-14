9dc802650ef3efcf68cc735f1103e24c7e5bfc75
mhd-thameem <mhdthameem595@gmail.com>
2026-08-14 16:19:02 +0530

    BST

---
+++ DAY 27/findMinInTree.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+};
+int findMin(TreeNode* root){
+    if(root->left==nullptr)return root->val;
+    return findMin(root->left);
+}
+int main(){
+    TreeNode* c1=new TreeNode(5);
+    TreeNode* c2=new TreeNode(3);
+    TreeNode* c3=new TreeNode(8);
+    TreeNode* c4=new TreeNode(1);
+    TreeNode* c5=new TreeNode(4);
+    TreeNode* c6=new TreeNode(9);
+    c1->left=c2;
+    c1->right=c3;
+    c2->left=c4;
+    c2->right=c5;
+    c3->right=c6;
+    cout<<findMin(c1);
+    return 0;
+}
