8bb54c81805c0f20089811ae07839d1a2a0ef7da
mhd-thameem <mhdthameem595@gmail.com>
2026-08-26 16:02:57 +0530

    LC Day 100

---
+++ F2/LCDaily100.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+        string ans = "";
+        
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = i; j < n; j++) {
+                if (s[j] == '1') {
+                    count++;
+                }
+                
+                if (count == k) {
+                    string sub = s.substr(i, j - i + 1);
+                    if (ans == "" || sub.length() < ans.length() || (sub.length() == ans.length() && sub < ans)) {
+                        ans = sub;
+                    }
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
